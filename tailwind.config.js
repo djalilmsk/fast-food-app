a3978f2e320c849396a9a45113a3e00f714c9f18
Abd Eldjallil Meskali <155311349+djalilmsk@users.noreply.github.com>
2025-11-02 22:07:58 +0100

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FE8C00',
+        white: {
+          DEFAULT: '#ffffff',
+          100: '#fafafa',
+          200: '#FE8C00',
+        },
+        gray: {
+          100: '#878787',
+          200: '#878787',
+        },
+        dark: {
+          100: '#181C2E',
+        },
+        error: '#F14141',
+        success: '#2F9B65',
+      },
+      fontFamily: {
+        quicksand: ['quicksand', 'sans-serif'],
+        'quicksand-bold': ['quicksand-bold', 'sans-serif'],
+        'quicksand-semibold': ['quicksand-semibold', 'sans-serif'],
+        'quicksand-light': ['quicksand-light', 'sans-serif'],
+        'quicksand-medium': ['quicksand-medium', 'sans-serif'],
+        'rubik-extrabold': ['rubik-extrabold', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
